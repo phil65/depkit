b609a5c59a522fba67c2e80b2fb6249726227a55
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 16:26:28 +0200

    chore: typing

--- src/depkit/__init__.py
+++ src/depkit/__init__.py
@@ -5,12 +5,13 @@ from importlib.metadata import version
 __version__ = version("depkit")
 
 from depkit.depmanager import DependencyManager
-from depkit.exceptions import (
-    DependencyError,
-    ScriptError,
-    ImportPathError,
-)
+from depkit.exceptions import DependencyError, ScriptError, ImportPathError
 
 
 __all__ = [
-    "__version__","DependencyError", "DependencyManager", "ImportPathError", "ScriptError"]
+    "DependencyError",
+    "DependencyManager",
+    "ImportPathError",
+    "ScriptError",
+    "__version__",
+]
