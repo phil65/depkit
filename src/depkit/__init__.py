15aff70dedbfad1df29a0bc0c43c4c8a132df5b2
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 18:03:46 +0200

    chore: bump template

--- src/depkit/__init__.py
+++ src/depkit/__init__.py
@@ -1,8 +1,17 @@
+"""DepKit: Tools to manage (uv) environemnts programmatically."""
+
 from __future__ import annotations
 
 from importlib.metadata import version
 
 __version__ = version("depkit")
+__title__ = "DepKit"
+__description__ = "Tools to manage (uv) environemnts programmatically."
+__author__ = "Philipp Temminghoff"
+__author_email__ = "philipptemminghoff@googlemail.com"
+__copyright__ = "Copyright (c) 2024 Philipp Temminghoff"
+__license__ = "MIT"
+__url__ = "https://github.com/phil65/depkit"
 
 from depkit.depmanager import DependencyManager
 from depkit.exceptions import DependencyError, ScriptError, ImportPathError
