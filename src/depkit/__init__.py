a533a209537159e5a7468da9e3db7613db9f91f9
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 05:25:02 +0200

    fix: resolve __init__.py merge conflict from template update

--- src/depkit/__init__.py
+++ src/depkit/__init__.py
@@ -1,4 +1,8 @@
-__version__ = "0.4.3"
+from __future__ import annotations
+
+from importlib.metadata import version
+
+__version__ = version("depkit")
 
 from depkit.depmanager import DependencyManager
 from depkit.exceptions import (
@@ -8,4 +12,5 @@ from depkit.exceptions import (
 )
 
 
-__all__ = ["DependencyError", "DependencyManager", "ImportPathError", "ScriptError"]
+__all__ = [
+    "__version__","DependencyError", "DependencyManager", "ImportPathError", "ScriptError"]
